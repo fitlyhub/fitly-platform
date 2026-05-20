55c5edc62503010c0e586e7f2b1263caca349ba7
fitly.zero <duyny.contact@gmail.com>
2026-05-20 14:24:49 +0700

    feat: add system database schema

---
+++ script/struct/1.sys_table.sql
@@ -0,0 +1,138 @@
+create table if not exists sys_orgtype
+(
+    sys_orgtype_id uuid                    not null
+        primary key,
+    code           varchar(100)            not null
+        unique,
+    name           varchar(255)            not null
+        unique,
+    is_active      boolean   default true  not null,
+    created_at     timestamp default now() not null,
+    updated_at     timestamp default now() not null,
+    created_by     uuid,
+    updated_by     uuid
+);
+
+create table if not exists sys_org
+(
+    sys_org_id      uuid                    not null
+        primary key,
+    parent_org_id   uuid,
+    code            varchar(100)            not null
+        unique,
+    name            varchar(255)            not null,
+    sys_orgtype_id  uuid
+        references sys_orgtype,
+    search_path     text,
+    is_legal_entity boolean   default false not null,
+    is_active       boolean   default true  not null,
+    created_at      timestamp default now() not null,
+    updated_at      timestamp default now() not null,
+    created_by      uuid,
+    updated_by      uuid
+);
+
+create table if not exists sys_position
+(
+    sys_position_id uuid                    not null
+        primary key,
+    code            varchar(100)            not null,
+    name            varchar(255)            not null,
+    description     text,
+    is_active       boolean   default true  not null,
+    created_at      timestamp default now() not null,
+    updated_at      timestamp default now() not null,
+    created_by      uuid,
+    updated_by      uuid,
+    sys_org_id      uuid                    not null
+        references sys_org
+);
+
+create table if not exists sys_user
+(
+    sys_user_id   uuid                    not null
+        primary key,
+    username      varchar(255)            not null
+        unique,
+    password      varchar(255)            not null,
+    first_name    varchar(255),
+    last_name     varchar(255),
+    full_name     varchar(512),
+    email         varchar(255),
+    phone         varchar(50),
+    avatar_url    text,
+    is_active     boolean   default true  not null,
+    is_locked     boolean   default false not null,
+    last_login_at timestamp,
+    created_at    timestamp default now() not null,
+    updated_at    timestamp default now() not null,
+    created_by    uuid,
+    updated_by    uuid
+);
+
+create table if not exists sys_segment
+(
+    sys_segment_id    uuid                    not null
+        primary key,
+    parent_segment_id uuid
+        references sys_segment,
+    code              varchar(64)             not null,
+    name              varchar(255)            not null,
+    search_path       text,
+    is_active         boolean   default true  not null,
+    created_at        timestamp default now() not null,
+    updated_at        timestamp default now() not null,
+    created_by        uuid                    not null,
+    updated_by        uuid                    not null
+);
+
+create table if not exists sys_role
+(
+    sys_role_id uuid                    not null
+        primary key,
+    code        varchar(100)            not null,
+    name        varchar(255)            not null,
+    description text,
+    is_active   boolean   default true  not null,
+    created_at  timestamp default now() not null,
+    updated_at  timestamp default now() not null,
+    created_by  uuid                    not null,
+    updated_by  uuid                    not null
+);
+
+create table if not exists sys_user_position
+(
+    sys_user_position_id uuid                    not null
+        primary key,
+    sys_user_id          uuid                    not null
+        references sys_user,
+    sys_position_id      uuid                    not null
+        references sys_position,
+    is_default           boolean   default false not null,
+    is_active            boolean   default true  not null,
+    created_at           timestamp default now() not null,
+    updated_at           timestamp default now() not null,
+    created_by           uuid                    not null,
+    updated_by           uuid                    not null
+);
+
+create table if not exists sys_position_role_scope
+(
+    sys_position_role_scope_id uuid                    not null
+        primary key,
+    sys_position_id            uuid                    not null
+        references sys_position,
+    sys_role_id                uuid                    not null
+        references sys_role,
+    org_scope                  varchar(100)            not null,
+    sys_org_id                 uuid
+        references sys_org,
+    segment_scope              varchar(100)            not null,
+    sys_segment_id             uuid
+        references sys_segment,
+    is_active                  boolean   default true  not null,
+    created_at                 timestamp default now() not null,
+    updated_at                 timestamp default now() not null,
+    created_by                 uuid                    not null,
+    updated_by                 uuid                    not null
+);
