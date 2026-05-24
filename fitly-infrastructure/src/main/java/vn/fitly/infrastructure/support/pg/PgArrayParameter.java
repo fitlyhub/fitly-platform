54f50f0fe0cf85da46fdab86cdc810d2b8494dda
fitly.zero <duyny.contact@gmail.com>
2026-05-24 21:30:24 +0700

    processing (upload for keep data)

--- fitly-infrastructure/src/main/java/vn/fitly/infrastructure/support/pg/PgArrayParameter.java
+++ fitly-infrastructure/src/main/java/vn/fitly/infrastructure/support/pg/PgArrayParameter.java
@@ -1,10 +1,6 @@
 /**
- * Project: Fitly Platform
- * Author:  fitly.zero
- * Date:    May 24, 2026
- * Time:    2:54:00 PM
- * * Copyright (c) 2026 fitly.zero. All rights reserved.
- * Licensed under the Apache License 2.0.
+ * Project: Fitly Platform Author:  fitly.zero Date:    May 24, 2026 Time:    2:54:00 PM * Copyright (c) 2026
+ * fitly.zero. All rights reserved. Licensed under the Apache License 2.0.
  */
 package vn.fitly.infrastructure.support.pg;
 
@@ -14,35 +10,35 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * 
+ *
  */
 public class PgArrayParameter {
 
     private final PgObjectType type;
 
-    private final List<Object> dataList = new ArrayList<>();;
+    private final List<Object> dataList = new ArrayList<>();
+    ;
 
-    private PgArrayParameter(PgObjectType type, Collection<Object> dataList) {
+    private PgArrayParameter(PgObjectType type, Collection<? extends Object> dataList) {
         this.type = type;
         this.dataList.addAll(dataList);
 
     }
-    
-    public static setArrayInt(Collection)
 
-    public PgArrayParameter(Collection<Integer> dataList) {
-        this.type = PgObjectType.INT4;
-        this.objectList.addAll(objectList);
+    public static PgArrayParameter setArrayInt(Collection<Integer> dataList) {
+        return new PgArrayParameter(PgObjectType.INT4, dataList);
     }
 
-    public PgArrayParameter(Collection<Long> dataList) {
-        this.type = PgObjectType.INT8;
-        this.objectList.addAll(objectList);
+    public static PgArrayParameter setArrayLong(Collection<Long> dataList) {
+        return new PgArrayParameter(PgObjectType.INT8, dataList);
     }
 
-    public PgArrayParameter(Collection<UUID> dataList) {
-        this.type = PgObjectType.UUID;
-        this.objectList.addAll(objectList);
+    public static PgArrayParameter setArrayString(Collection<String> dataList) {
+        return new PgArrayParameter(PgObjectType.VARCHAR, dataList);
+    }
+
+    public static PgArrayParameter setArrayUUID(Collection<UUID> dataList) {
+        return new PgArrayParameter(PgObjectType.UUID, dataList);
     }
 
 }
